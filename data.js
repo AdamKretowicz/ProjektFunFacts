3749173c7261e88d04074977cfdc7e85d718f264
AdamKretowicz <adamkretowicz99@gmail.com>
2025-01-24 01:24:13 +0100

    fetching facts from api

--- data.js
+++ data.js
@@ -32,11 +32,43 @@ const initialFacts = [
   },
 ];
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 const factsList = document.querySelector(".facts-list");
 
 factsList.innerHTML = "";
+
+loadFacts();
+
+async function loadFacts() {
+  const res = await fetch(
+    "https://dsgsekmmkesxcfcdelzk.supabase.co/rest/v1/facts",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzZ3Nla21ta2VzeGNmY2RlbHprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NDQyNzIsImV4cCI6MjA1MzIyMDI3Mn0.tDvrwQMh3p4R6Ky6jS11aOfe5fjLkvi2YizOgcPGYcQ",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzZ3Nla21ta2VzeGNmY2RlbHprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NDQyNzIsImV4cCI6MjA1MzIyMDI3Mn0.tDvrwQMh3p4R6Ky6jS11aOfe5fjLkvi2YizOgcPGYcQ",
+      },
+    }
+  );
+
+  const data = await res.json();
+
+  createFactsList(data);
+}
+
 createFactsList(initialFacts);
 
 function createFactsList(dataArray) {
@@ -46,7 +78,9 @@ function createFactsList(dataArray) {
       ${fact.text}
       <a class="source" href="${fact.source}" target="_blank">(Source)</a>
     </p>
-    <span class="tag" style="background-color: #3b82f6">${fact.category}</span>
+    <span class="tag" style="background-color: ${
+      CATEGORIES.find((cat) => cat.name === fact.category).color
+    }">${fact.category}</span>
   </li>`
   );
   const html = htmlArr.join("");
